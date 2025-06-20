96bfae1e13f1a9a7ee24a1087885a088b15f678e
sabil92 <sabnufauzan@gmail.com>
2025-06-20 18:59:30 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,9 @@
-const cacheName = 'veo3-cache-v1';
-const filesToCache = [
-  'index.html',
-  'manifest.json',
-  'icon-192.png',
-  'icon-512.png'
-];
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+const cacheName = 'veo3-surabaya-v1';
+const filesToCache = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
 });
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
